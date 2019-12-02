181bd124678ac743e2a3e0fe8be4b7d92ff340c7
anapayanruiz <anapayan22@gmail.com>
2019-12-02 11:09:20 +0100

    WIP in function paintShowsFavorites

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
 # Tareas evaluacion final-modulo 2-anapayanruiz
 
-- [] Fetch para traer datos
-- [] Parsear o adapatar los datos del servidor
-- [] Pintar series
-- [] Marcar series favoritas
-      - [] Escuchar click en una serie favorita
-      - [] Identificar que serie es
-      - [] Guardar las paletas favoritas en otro array
-      - [] Añadir o quitar la clase favorite en funcion de si es favorita
-      - [] Quitar la clase de favoritos
+- [x] Fetch para traer datos
+- [x] Parsear o adapatar los datos del servidor
+- [x] Pintar series
+- [] Marcar series favoritas - [] Escuchar click en una serie favorita - [] Identificar que serie es - [] Guardar las paletas favoritas en otro array - [] Añadir o quitar la clase favorite en funcion de si es favorita - [] Quitar la clase de favoritos
 - [] Cachear datos del servidor en local storage
 - [] Guardar en local storage
 - [] Bonus: Guardar en local storage los favoritos
--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,7 +6,8 @@ shows[]
     showsItem{}
             show{}
             show.name
-            show.image
+            show.image{}
+                show.image.medium
 */
 
 
@@ -15,37 +16,118 @@ shows[]
 
 let shows = [];
 
+let showsFavorites = [];
+
 
 
 //funcion para pintar series
-
 function paintShows() {
   const searchResult = document.querySelector('.show-result__list');
   let htmlCode = '';
-  let showImage = '';
-  //guardo en constantes el nombre
-  const showName = showsItem.show.name;
-
-
+  console.log(shows);
 
   //tengo que recorrer el array shows
   for (const showsItem of shows) {
+    const showName = showsItem.show.name;
+    //aqui guardo la posicion que tiene la serie
+    const showIndex = shows.indexOf(showsItem);
+    //aqui guardo el id que tiene la serie
+    const showId = showsItem.show.id;
+    let showImage = '';
 
-    if (showsItem.show.image.medium) {
+    //si no tengo imagen tengo que darle una condicion para que aparezca una imagen por defecto
+    if (showsItem.show.image !== null) {
+      showImage = showsItem.show.image.medium;
 
+    } else {
+      showImage = "https://via.placeholder.com/210x295.png";
     }
 
-
     htmlCode += `<li>`;
-    htmlCode += `<button><img src="${showImage}"/><h3>${showName}</h3></button>`;
+    htmlCode += `<button class="show-result__button" data-index="${showIndex}" data-id="${showId}">
+                    <img class="show-result__image" src="${showImage}"/>
+                    <h3 class="show-result__title">${showName}</h3>
+                </button>`;
     htmlCode += `</li>`;
 
   }
 
   searchResult.innerHTML = htmlCode;
 
+
+  //listener para selecionar series favoritas
+  //dentro de pintar porque inmediatamente va detrás. 
+  //pintamos y escuchamos 
+  const listElements = document.querySelectorAll('.show-result__button');
+  for (const listElement of listElements) {
+    listElement.addEventListener('click', handleFavoriteShows);
+  }
+
 }
 
+
+//handle de series favoritas
+function handleFavoriteShows(event) {
+  showsFavorites.push(event.currentTarget.dataset.id);
+  paintFavoriteShows();
+
+
+  const clickedId = event.currentTarget.dataset.id
+  const favoriteIndex = showsFavorites.indexOf(clickedId);
+  console.log('selecciono fav id;', clickedId, 'selecciono fav index:', favoriteIndex);
+  const isFavorite = favoriteIndex !== -1;
+
+  console.log('Favs:', showsFavorites, 'clicked', clickedId, '¿ES favorito?', isFavorite);
+
+
+
+
+}
+
+
+
+
+
+//funcion para pintar series favoritas
+function paintFavoriteShows() {
+
+  const searchResult = document.querySelector('.show-result__list');
+  let htmlCode = '';
+  console.log(shows);
+
+  //tengo que recorrer el array shows
+  for (const showsItem of shows) {
+    const showName = showsItem.show.name;
+    //aqui guardo la posicion que tiene la serie
+    const showIndex = shows.indexOf(showsItem);
+    //aqui guardo el id que tiene la serie
+    const showId = showsItem.show.id;
+    let showImage = '';
+
+    //si no tengo imagen tengo que darle una condicion para que aparezca una imagen por defecto
+    if (showsItem.show.image !== null) {
+      showImage = showsItem.show.image.medium;
+
+    } else {
+      showImage = "https://via.placeholder.com/210x295.png";
+    }
+
+    htmlCode += `<li>`;
+    htmlCode += `<button class="show-result__button" data-index="${showIndex}" data-id="${showId}">
+                      <img class="show-result__image" src="${showImage}"/>
+                      <h3 class="show-result__title">${showName}</h3>
+                  </button>`;
+    htmlCode += `</li>`;
+
+  }
+
+  searchResult.innerHTML = htmlCode;
+
+
+
+
+
+}
 
 
 
@@ -69,8 +151,8 @@ function getServerData(event) {
     .then(function (serverData) {
       //adapto los datos del servidor para usarlos
       shows = serverData;
-      console.log(serverData);
       paintShows();
+      paintFavoriteShows();
     })
     .catch(function (err) {
       console.log('error', err);
