cd60a78612da2b5fba420b7c6cc88fd14d1cba12
anapayanruiz <anapayan22@gmail.com>
2019-12-02 10:09:09 +0100

    WIP in function paintShows

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Tareas evaluacion final-modulo 2-anapayanruiz
+
+- [] Fetch para traer datos
+- [] Parsear o adapatar los datos del servidor
+- [] Pintar series
+- [] Marcar series favoritas
+      - [] Escuchar click en una serie favorita
+      - [] Identificar que serie es
+      - [] Guardar las paletas favoritas en otro array
+      - [] Añadir o quitar la clase favorite en funcion de si es favorita
+      - [] Quitar la clase de favoritos
+- [] Cachear datos del servidor en local storage
+- [] Guardar en local storage
+- [] Bonus: Guardar en local storage los favoritos
--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,85 @@
 'use strict';
 
-console.log('>> Ready :)');
+
+/* estructura array del servidor 
+shows[]
+    showsItem{}
+            show{}
+            show.name
+            show.image
+*/
+
+
+
+//Array donde volcamos las series
+
+let shows = [];
+
+
+
+//funcion para pintar series
+
+function paintShows() {
+  const searchResult = document.querySelector('.show-result__list');
+  let htmlCode = '';
+  let showImage = '';
+  //guardo en constantes el nombre
+  const showName = showsItem.show.name;
+
+
+
+  //tengo que recorrer el array shows
+  for (const showsItem of shows) {
+
+    if (showsItem.show.image.medium) {
+
+    }
+
+
+    htmlCode += `<li>`;
+    htmlCode += `<button><img src="${showImage}"/><h3>${showName}</h3></button>`;
+    htmlCode += `</li>`;
+
+  }
+
+  searchResult.innerHTML = htmlCode;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//aqui llamamos al servidor
+function getServerData(event) {
+  event.preventDefault();
+  const searchInput = document.querySelector(".search-form__input");
+  fetch(`http://api.tvmaze.com/search/shows?q=${searchInput.value}`)
+
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (serverData) {
+      //adapto los datos del servidor para usarlos
+      shows = serverData;
+      console.log(serverData);
+      paintShows();
+    })
+    .catch(function (err) {
+      console.log('error', err);
+    });
+}
+
+
+//INIT
+//listener del servidor
+
+const searchButton = document.querySelector(".search-form__button");
+searchButton.addEventListener('click', getServerData);
